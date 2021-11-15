db099229097dd076701a713d470d566df26a8ddf
sujitIwale <56259235+sujitIwale@users.noreply.github.com>
2021-11-15 20:58:52 +0530

    feat : sidebar added

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,19 +5,77 @@ import './Navbar.css';
 const Navbar = () => {
 	const location = useLocation();
 	const isSelected = (path) => {
-		if (path === location.pathname) return 'selected';
+		if (path === location.pathname) return 'active';
 	};
 	return (
-		<div className='navbar'>
-			<ul>
-				<Link to='/todos'>
-					<li className={`${isSelected('/todos')}`}>Todo</li>
-				</Link>
-				<Link to='/notes'>
-					<li className={`${isSelected('/notes')}`}>Notes</li>
-				</Link>
-			</ul>
-		</div>
+		<>
+			{/* <div class='area'></div> */}
+			<nav class='main-menu'>
+				<ul>
+					<li>
+						<Link to='/'>
+							<i className={`fa fa-home ${isSelected('/')}`}></i>
+							<span class='nav-text'>Home</span>
+						</Link>
+					</li>
+					<li>
+						<Link to='/todos'>
+							<i
+								className={`fa fa-tasks ${isSelected(
+									'/todos'
+								)}`}></i>
+							<span class='nav-text'>Tasks</span>
+						</Link>
+					</li>
+					<li>
+						<Link to='/notes'>
+							<i
+								className={`fa fa-sticky-note ${isSelected(
+									'/notes'
+								)}`}></i>
+							<span class='nav-text'>Notes</span>
+						</Link>
+					</li>
+					<li>
+						<Link to='/projects'>
+							<i
+								className={`fa fa-laptop ${isSelected(
+									'/projects'
+								)}`}></i>
+							<span class='nav-text'>Projects</span>
+						</Link>
+					</li>
+					<li>
+						<Link to='/'>
+							<i
+								className={`fa fa-user-friends ${isSelected(
+									'/teams'
+								)}`}></i>
+							<span class='nav-text'>Teams</span>
+						</Link>
+					</li>
+					<li>
+						<Link to='/'>
+							<i
+								className={`fa fa-user ${isSelected(
+									'/account'
+								)}`}></i>
+							<span class='nav-text'>Account</span>
+						</Link>
+					</li>
+				</ul>
+
+				{/* <ul class='logout'>
+					<li>
+						<Link to='/'>
+							<i
+								className={`fa fa-home ${isSelected('/')}`}></i>
+							<span class='nav-text'>Home</span>
+						</Link>
+					</li>
+				</ul> */}
+			</nav>
+		</>
 	);
 };
 
